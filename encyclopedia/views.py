e872be7fa442776fac23a2edfd73a2a7ec1cb81b
LazyMan78 <demetra_n@front.ru>
2020-07-06 14:41:16 +0300

    добавлено entry page условие

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
+from django.http import Http404
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -6,5 +8,14 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def page(request, name):
+    page = markdown2.markdown( util.get_entry(name))
+    if not page:
+        raise Http404("Page does not exist")
+    return render(request, "encyclopedia/page.html", {
+        "title":name,
+        "content": page
     })
 
