d62c2565234f3a25529fe814c89b6901f886f125
LazyMan78 <demetra_n@front.ru>
2020-07-06 15:55:31 +0300

    добавлено search условие

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:name>", views.page, name='page')
+    path("wiki/<str:name>", views.page, name='page'),
+    path("wiki", views.found_page, name='found_page')
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 import markdown2
+from django.urls import reverse
 
 from . import util
 
@@ -11,6 +12,9 @@ def index(request):
     })
 
 def page(request, name):
+    for x in util.list_entries():
+        if name.lower() == x.lower():
+            name = x
     page = markdown2.markdown( util.get_entry(name))
     if not page:
         raise Http404("Page does not exist")
@@ -19,3 +23,13 @@ def page(request, name):
         "content": page
     })
 
+def found_page(request):
+    if request.method == 'POST':
+        title = request.POST.get('q')
+        if title.lower() in [x.lower() for x in util.list_entries()]:
+            return HttpResponseRedirect(reverse("page", args=(title,)))
+        else:
+            pages = [page for page in util.list_entries() if title.lower() in page.lower()]
+            return render(request, "encyclopedia/found_pages.html", {
+                "entries": pages
+            })
